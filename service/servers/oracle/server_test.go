17a384320a782897c63b45613087c3f4c0970911
swelf <golishkin@gmail.com>
2025-12-09 00:22:16 +0300

    fixed unit test

--- service/servers/oracle/server_test.go
+++ service/servers/oracle/server_test.go
@@ -74,6 +74,10 @@ func (s *ServerTestSuite) SetupTest() {
 	// start server + client w/ context
 	go s.srv.StartServer(s.ctx, localhost, port)
 
+	// TODO: refactor
+	// give a time to server to open socket
+	time.Sleep(1 * time.Second)
+
 	dialCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	s.Require().NoError(s.client.Start(dialCtx))
